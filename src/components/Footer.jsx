3dc39ac4aea9c7abcc7a4255f720d214e81b569a
Girlyver C. Jandayan <girlyverjandayan@gmail.com>
2025-02-23 16:09:01 +0800

    new push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,17 @@ const Footer = () => {
         
             <h1 className="flex justify-center translate-y-[-20px]">Follow me:</h1>
 
-            <img className="cursor-pointer w-[40px] h-[40px] translate-y-[-10px] translate-x-[660px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231322/portfolio/vmq14lcjmgntz10hrnao.png" alt="facebook" />
-            <img className="cursor-pointer w-[60px] h-[40px] translate-x-[725px] translate-y-[-48px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231323/portfolio/w19kmmz7jrfkc37oq2cw.png" alt="instagram" />
-            <img className="cursor-pointer w-[90px] h-[50px] translate-x-[780px] translate-y-[-90px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231322/portfolio/bmsditgm9jgzpllzwyme.png" alt="github" />
+            <a href="https://www.facebook.com/gvjandayan12" target="_blank">
+                <img className="cursor-pointer w-[40px] h-[40px] translate-y-[-10px] translate-x-[660px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231322/portfolio/vmq14lcjmgntz10hrnao.png" alt="facebook" />
+            </a>
+
+            <a href="https://www.instagram.com/girlyver_/" target="_blank">
+                <img className="cursor-pointer w-[60px] h-[40px] translate-x-[725px] translate-y-[-48px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231323/portfolio/w19kmmz7jrfkc37oq2cw.png" alt="instagram" />
+            </a>
+
+            <a href="https://github.com/gvjandayan12" target="_blank">
+                <img className="cursor-pointer w-[90px] h-[50px] translate-x-[780px] translate-y-[-90px]" src="https://res.cloudinary.com/dncasxk1e/image/upload/v1740231322/portfolio/bmsditgm9jgzpllzwyme.png" alt="github" />
+            </a>
 
 
             <h1 className="translate-x-[450px] text-[25px] w-[150px] translate-y-[-170px]">Contact:</h1>
